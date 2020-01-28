314b8d8a5ca73dfe60047276e45373204a3371ab
Isaac Sanchez <luke_shakz@hotmail.com>
2020-01-27 18:52:06 -0800

    Update lambda_maps.py

--- Python/basic/lambda_maps.py
+++ Python/basic/lambda_maps.py
@@ -3,7 +3,12 @@ mi_funcion = lambda numero: numero * numero
 lista = [1, 2, 3, 4, 5]
 #resultado = map(lambda numero: numero * numero , lista) en una sola linea todo
 
-resultado = map(mi_funcion, lista)
-lista_resultado = list(resultado)
-print(lista_resultado)
+# resultado = map(mi_funcion, lista)
+# lista_resultado = list(resultado)
+# print(lista_resultado)
 
+for i in reversed(lista):
+    print(i)
+
+for j in range(len(lista), 0, -1):
+    print(j)
